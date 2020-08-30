16aa0c8619318cecba1f6ac4c9298bdd7370168c
Hmekus <eliasgotto@gmail.com>
2020-08-30 12:48:25 +0300

    Display function refactored, create the operate function, which evaluate single pairs of operands

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+let displayValue = document.getElementById('result');
+
+/*let matchItUp = {
+  '+': function (x, y) {return x + y},
+  '-': function (x, y) {return x - y}
+};*/
+
 function add(a, b) {
   let sum = a + b;
   return sum;
@@ -16,14 +23,39 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(operator, a, b) {
-  if (operator === '+') {
-    add(a, b);
-  } else if (operator === '-') {
-    subtract(a, b);
-  } else if (operator === '*') {
-    multiply(a, b);
-  } else {
-    divide(a, b);
+function operate() {
+  let operateValue = displayValue.value.split(' ');
+  let operands = [];
+  let operators = [];
+
+  for (let i = 0; i < operateValue.length; i++) {
+    if (i % 2 === 0) {
+      operands.push(operateValue[i]);
+
+    } else {
+      operators.push(operateValue[i]);
+    }
   }
+
+  if (operators[0] === "-") {
+    displayValue.value = subtract(operands[0], operands[1]);
+  
+  } else if (operators[0] === "+") {
+    displayValue.value = add(Number(operands[0]), Number(operands[1]));
+
+  } else if (operators[0] === "*") {
+    displayValue.value = Number(multiply(operands));
+
+  } else {
+    displayValue.value = divide(operands[0], operands[1]);
+  }
+}    
+
+
+function display(button) {
+  displayValue.value += button;
+}
+
+function clear() {
+  displayValue.value = 0;
 }
